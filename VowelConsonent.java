133fc8209305234b40d46c18e774f12ac7835f94
Anushanallap <anusha.n468@gmail.com>
2021-12-10 10:18:44 +0530

    add VowelConsonent file

---
+++ VowelConsonent.java
@@ -0,0 +1,13 @@
+public class VowelConsonent {
+
+    public static void main(String[] args) {
+
+        char ch = 'v';
+
+        if(ch == 'v' || ch == 'e' || ch == 'n' || ch == 'u'  )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonent");
+
+    }
+}
